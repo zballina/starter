0c32244fb9efe6cd4fae98f9d6b3b51615f0998d
francisco.ballina <zballina@gmail.com>
2026-08-15 20:08:52 -0600

    feat: complete NvChad setup with LSPs, formatting, Mason tools and img-clip

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,20 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    python = { "isort", "black" },
+    javascript = { "prettierd", "prettier", stop_after_first = true },
+    typescript = { "prettierd", "prettier", stop_after_first = true },
+    typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+    json = { "prettierd", "prettier", stop_after_first = true },
+    yaml = { "prettierd", "prettier", stop_after_first = true },
+    sh = { "shfmt" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  -- Habilitar formateo automático al guardar
+  format_on_save = {
+    timeout_ms = 1000,
+    lsp_fallback = true,
+  },
 }
 
 return options
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,37 @@
+-- Cargar los defaults de NvChad (diagnósticos, floating windows, bordes)
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
-vim.lsp.enable(servers)
+local nvlsp = require "nvchad.configs.lspconfig"
 
--- read :h vim.lsp.config for changing options of lsp servers 
+local servers = {
+  "html",
+  "cssls",
+  "ts_ls",
+  "pyright",
+  "bashls",
+  "dockerls",
+  "yamlls",
+  "lua_ls",
+}
+
+-- Configuración limpia usando vim.lsp (evita el deprecation warning de lspconfig)
+for _, lsp in ipairs(servers) do
+  if vim.lsp and vim.lsp.config and vim.lsp.enable then
+    -- API nativa de Neovim 0.11+
+    vim.lsp.config[lsp] = {
+      cmd = nil,
+      on_attach = nvlsp.on_attach,
+      on_init = nvlsp.on_init,
+      capabilities = nvlsp.capabilities,
+    }
+    vim.lsp.enable(lsp)
+  else
+    -- Fallback para versiones anteriores usando lspconfig clásico
+    local lspconfig = require "lspconfig"
+    lspconfig[lsp].setup {
+      on_attach = nvlsp.on_attach,
+      on_init = nvlsp.on_init,
+      capabilities = nvlsp.capabilities,
+    }
+  end
+end
